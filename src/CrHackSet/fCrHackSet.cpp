6589661cda0ab6fb86d1a991b1e722aa3253387a
prefetchnta <crhackos@gmail.com>
2013-03-22 15:50:51 +0800

    CrHackSet: 增加一组像素运算滤镜

--- src/CrHackSet/fCrHackSet.cpp
+++ src/CrHackSet/fCrHackSet.cpp
@@ -1,2 +1,202 @@
 
 #include "../QstLibs/QstLibs.h"
+
+/*
+---------------------------------------
+    图片逻辑-与
+---------------------------------------
+*/
+static bool_t
+fill_and_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = xml_attr_intx32U("mask", 0xFFFFFFFF, param);
+    fill_and32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    图片逻辑-或
+---------------------------------------
+*/
+static bool_t
+fill_orr_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = xml_attr_intx32U("mask", 0x00000000, param);
+    fill_orr32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    图片逻辑-非
+---------------------------------------
+*/
+static bool_t
+fill_not_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(param);
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = 0x00000000;
+    fill_not32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    图片逻辑异或
+---------------------------------------
+*/
+static bool_t
+fill_xor_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = xml_attr_intx32U("mask", 0x00000000, param);
+    fill_xor32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    图片饱和加法
+---------------------------------------
+*/
+static bool_t
+fill_add_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = xml_attr_intx32U("color", 0x00000000, param);
+    fill_add32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    图片饱和减法
+---------------------------------------
+*/
+static bool_t
+fill_sub_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = xml_attr_intx32U("color", 0x00000000, param);
+    fill_sub32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    图片插值填充
+---------------------------------------
+*/
+static bool_t
+fill_lrp_x86 (
+  __CR_UU__ void_t*     nouse,
+  __CR_IO__ void_t*     image,
+  __CR_IN__ sXNODEu*    param
+    )
+{
+    sFILL   fill;
+    cpix_t  clrs;
+    sIMAGE* dest;
+
+    CR_NOUSE(nouse);
+    dest = (sIMAGE*)image;
+    fill.dx = fill.dy = 0;
+    fill.dw = dest->position.ww;
+    fill.dh = dest->position.hh;
+    clrs.val = xml_attr_intx32U("color", 0x00000000, param);
+    fill_lrp32_c(dest, &fill, clrs, NULL);
+    return (TRUE);
+}
+
+/*
+=======================================
+    滤镜接口导出表
+=======================================
+*/
+CR_API const sXC_PORT   qst_v2d_filter[] =
+{
+    { "crhack_and_x86", fill_and_x86 },
+    { "crhack_orr_x86", fill_orr_x86 },
+    { "crhack_not_x86", fill_not_x86 },
+    { "crhack_xor_x86", fill_xor_x86 },
+    { "crhack_add_x86", fill_add_x86 },
+    { "crhack_sub_x86", fill_sub_x86 },
+    { "crhack_lrp_x86", fill_lrp_x86 },
+    { NULL, NULL },
+};
--- src/CrHackSet/xCrHackSet.cpp
+++ src/CrHackSet/xCrHackSet.cpp
@@ -22,17 +22,35 @@
 
 #include "../QstLibs/QstLibs.h"
 
+/* 外部库引用 */
+#ifndef _CR_NO_PRAGMA_LIB_
+    #if defined(_CR_CC_BCC_)
+        #pragma comment (lib, "CrH_BLIT_bcc.lib")
+    #else
+        #pragma comment (lib, "CrH_BLIT_msc.lib")
+    #endif
+#endif
+
 /*
 =======================================
     插件 DLL 入口点
 =======================================
 */
+#if defined(_CR_CC_BCC_)
+int  WINAPI
+DllEntryPoint (
+  __CR_IN__ HINSTANCE       hinst,
+  __CR_IN__ unsigned long   reason,
+  __CR_UU__ void*           reserved
+    )
+#else
 BOOL WINAPI
 DllMain (
   __CR_IN__ HANDLE  hinst,
   __CR_IN__ DWORD   reason,
   __CR_UU__ LPVOID  reserved
     )
+#endif
 {
     switch (reason)
     {
