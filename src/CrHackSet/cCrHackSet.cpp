91ae0bfed067ae2fae8fbde3782710190ff304ad
prefetchnta <crhackos@gmail.com>
2013-04-10 11:45:09 +0800

    CrHackSet: 图块输出命令支持对齐功能

--- src/CrHackSet/cCrHackSet.cpp
+++ src/CrHackSet/cCrHackSet.cpp
@@ -992,7 +992,65 @@ qst_crh_loadres (
 
 /*
 ---------------------------------------
-    绘制图块
+    绘制图块 (内部实现)
+---------------------------------------
+*/
+static void_t
+qst_crh_blit_int (
+  __CR_IN__ sIMAGE*         draw,
+  __CR_IN__ sIMAGE*         srce,
+  __CR_IN__ const sBLIT*    blit
+    )
+{
+    switch (s_mode)
+    {
+        default:
+        case CR_BLT_SET:
+            if (s_trans.val == 0)
+                blit_set32_c(draw, srce, blit, NULL);
+            else
+                blit_str32_c(draw, srce, blit, s_trans, NULL);
+            break;
+
+        case CR_BLT_AND:
+            blit_and32_c(draw, srce, blit, NULL);
+            break;
+
+        case CR_BLT_ORR:
+            blit_orr32_c(draw, srce, blit, NULL);
+            break;
+
+        case CR_BLT_NOT:
+            if (s_trans.val == 0)
+                blit_alp32_c(draw, srce, NULL, blit, NULL);
+            else
+                blit_dtr32_c(draw, srce, blit, s_trans, NULL);
+            break;
+
+        case CR_BLT_XOR:
+            blit_xor32_c(draw, srce, blit, NULL);
+            break;
+
+        case CR_BLT_ADD:
+            blit_add32_c(draw, srce, blit, NULL);
+            break;
+
+        case CR_BLT_SUB:
+            blit_sub32_c(draw, srce, blit, NULL);
+            break;
+
+        case CR_BLT_ALP:
+            if (s_trans.val == 0)
+                blit_lrp32_c(draw, srce, blit, s_trans, FALSE, NULL);
+            else
+                blit_lrp32_c(draw, srce, blit, s_trans, TRUE, NULL);
+            break;
+    }
+}
+
+/*
+---------------------------------------
+    绘制图块 (支持对齐)
 ---------------------------------------
 */
 static bool_t
@@ -1004,11 +1062,15 @@ qst_crh_blit (
 {
     sBLIT   blit;
     sRECT   rect;
+    sRECT   dest;
+    sRECT   wins;
     int32u  idxs;
     sIMAGE* draw;
     sIMAGE* srce;
+    uint_t  ww,hh;
+    uint_t  align;
 
-    /* 参数解析 <X> <Y> <SpriteName> [Index] */
+    /* 参数解析 <X> <Y> <SpriteName> [Index] [Width Height Align] */
     if (argc < 4)
         return (FALSE);
     if (s_resx == NULL)
@@ -1025,56 +1087,83 @@ qst_crh_blit (
     srce = egui_res_get_img(s_resx, argv[3], idxs, &rect);
     if (srce == NULL)
         return (FALSE);
+
+    /* 计算对齐输出图块 */
+    dest.x1 = (sint_t)str2intxA(argv[1]);
+    dest.y1 = (sint_t)str2intxA(argv[2]);
+    rect_set_wh(&dest, dest.x1, dest.y1, rect.ww, rect.hh);
+    if (argc > 7) {
+        ww = str2intxA(argv[5]);
+        hh = str2intxA(argv[6]);
+        align = str2intxA(argv[7]);
+        rect_set_wh(&wins, dest.x1, dest.y1, ww, hh);
+        egui_rect_align(&dest, &wins, align);
+    }
+    blit.dx = dest.x1;
+    blit.dy = dest.y1;
     blit.sx = rect.x1;
     blit.sy = rect.y1;
-    blit.sw = rect.ww;
-    blit.sh = rect.hh;
-    blit.dx = (sint_t)str2intxA(argv[1]);
-    blit.dy = (sint_t)str2intxA(argv[2]);
-    switch (s_mode)
-    {
-        default:
-        case CR_BLT_SET:
-            if (s_trans.val == 0)
-                blit_set32_c(draw, srce, &blit, NULL);
-            else
-                blit_str32_c(draw, srce, &blit, s_trans, NULL);
-            break;
+    blit.sw = dest.ww;
+    blit.sh = dest.hh;
+    qst_crh_blit_int(draw, srce, &blit);
+    return (TRUE);
+}
 
-        case CR_BLT_AND:
-            blit_and32_c(draw, srce, &blit, NULL);
-            break;
+/*
+---------------------------------------
+    绘制图块 (支持对齐)
+---------------------------------------
+*/
+static bool_t
+qst_crh_blit2 (
+  __CR_IN__ void_t*     parm,
+  __CR_IN__ uint_t      argc,
+  __CR_IN__ ansi_t**    argv
+    )
+{
+    sBLIT   blit;
+    sRECT   rect;
+    sRECT   dest;
+    sRECT*  wins;
+    int32u  idxs;
+    uint_t  algn;
+    sIMAGE* draw;
+    sIMAGE* srce;
 
-        case CR_BLT_ORR:
-            blit_orr32_c(draw, srce, &blit, NULL);
-            break;
+    /* 参数解析 <SpriteName> <RectName> <Align> [Index] */
+    if (argc < 4)
+        return (FALSE);
+    if (s_resx == NULL)
+        return (FALSE);
+    draw = ((sQstView2D*)parm)->paint;
+    if (draw == NULL)
+        return (FALSE);
 
-        case CR_BLT_NOT:
-            if (s_trans.val == 0)
-                blit_alp32_c(draw, srce, NULL, &blit, NULL);
-            else
-                blit_dtr32_c(draw, srce, &blit, s_trans, NULL);
-            break;
+    /* 查找目标矩形对象 */
+    wins = egui_res_get_rct(s_resx, argv[2]);
+    if (wins == NULL)
+        return (FALSE);
 
-        case CR_BLT_XOR:
-            blit_xor32_c(draw, srce, &blit, NULL);
-            break;
+    /* 查找目标图片对象 */
+    if (argc > 4)
+        idxs = str2intxA(argv[4]);
+    else
+        idxs = 0;
+    srce = egui_res_get_img(s_resx, argv[1], idxs, &rect);
+    if (srce == NULL)
+        return (FALSE);
 
-        case CR_BLT_ADD:
-            blit_add32_c(draw, srce, &blit, NULL);
-            break;
-
-        case CR_BLT_SUB:
-            blit_sub32_c(draw, srce, &blit, NULL);
-            break;
-
-        case CR_BLT_ALP:
-            if (s_trans.val == 0)
-                blit_lrp32_c(draw, srce, &blit, s_trans, FALSE, NULL);
-            else
-                blit_lrp32_c(draw, srce, &blit, s_trans, TRUE, NULL);
-            break;
-    }
+    /* 计算对齐输出图块 */
+    algn = str2intxA(argv[3]);
+    rect_set_wh(&dest, wins->x1, wins->y1, rect.ww, rect.hh);
+    egui_rect_align(&dest, wins, algn);
+    blit.dx = dest.x1;
+    blit.dy = dest.y1;
+    blit.sx = rect.x1;
+    blit.sy = rect.y1;
+    blit.sw = dest.ww;
+    blit.sh = dest.hh;
+    qst_crh_blit_int(draw, srce, &blit);
     return (TRUE);
 }
 
@@ -1163,10 +1252,10 @@ qst_crh_text_ex (
     bool_t  free;
     bool_t  rett;
     bool_t  tran;
+    uint_t  algn;
     ansi_t* temp;
     ansi_t* text;
     sIMAGE* draw;
-    uint_t  align;
 
     /* 参数解析 <EscText/TextName> <RectName> <Align> [IsTextName] */
     if (argc < 4)
@@ -1208,8 +1297,8 @@ qst_crh_text_ex (
         tran = FALSE;
     else
         tran = TRUE;
-    align = str2intxA(argv[3]);
-    rett = qst_crh_text_int(draw, tran, rect, text, align);
+    algn = str2intxA(argv[3]);
+    rett = qst_crh_text_int(draw, tran, rect, text, algn);
     if (free)
         mem_free(text);
     return (rett);
@@ -1246,6 +1335,7 @@ CR_API const sQST_CMD   qst_v2d_cmdz[] =
     { "crh:trans", qst_crh_trans },
     { "crh:loadres", qst_crh_loadres },
     { "crh:blit", qst_crh_blit },
+    { "crh:blit2", qst_crh_blit2 },
     { "crh:rect_ex", qst_crh_rect_ex },
     { "crh:fill_ex", qst_crh_fill_ex },
     { "crh:texts_ex", qst_crh_text_ex },
