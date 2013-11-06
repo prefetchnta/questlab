12036b334af9721b185fb9457f8eb675cb906d8f
prefetchnta <crhackos@gmail.com>
2013-11-06 20:47:07 +0800

    MAKE: 删除内部项目目录

--- c0950.bat
+++ c0950.bat
@@ -1,12 +1,10 @@
 @echo off
 attrib .\tmp +h
-attrib .\wrk +h
 attrib .\bin\ext3rd +h
 attrib .\bin\output +h
 attrib .\bin\source\tessdata +h
 cpage 950 65001 . *.*
 attrib .\tmp -h
-attrib .\wrk -h
 attrib .\bin\ext3rd -h
 attrib .\bin\output -h
 attrib .\bin\source\tessdata -h
--- delbom.bat
+++ delbom.bat
@@ -1,12 +1,10 @@
 @echo off
 attrib .\tmp +h
-attrib .\wrk +h
 attrib .\bin\ext3rd +h
 attrib .\bin\output +h
 attrib .\bin\source\tessdata +h
 cpage 0 0 . *.*
 attrib .\tmp -h
-attrib .\wrk -h
 attrib .\bin\ext3rd -h
 attrib .\bin\output -h
 attrib .\bin\source\tessdata -h
