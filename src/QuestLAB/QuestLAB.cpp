e003f584fbcd288a7d24b4bca73f909a16be4883
prefetchnta <crhackos@gmail.com>
2013-11-29 14:11:29 +0800

    QuestLAB: 采用覆盖的方式管理不同配置方案

--- src/QuestLAB/QuestLAB.cpp
+++ src/QuestLAB/QuestLAB.cpp
@@ -31,9 +31,6 @@
     #define WIN_XCONF   "QuestLAB.xml"
 #endif
 
-/* 默认配置名称 */
-#define DEF_CONF_NAME   "base"
-
 /*
 =======================================
     WinMain 程序入口
@@ -74,7 +71,7 @@ WinMain (
 
     /* 参数解析 [配置名称] */
     if (argc == 0 || str_lenA(argv[0]) > 64)
-        conf_name = DEF_CONF_NAME;
+        conf_name = NULL;
     else
         conf_name = argv[0];
 
@@ -82,46 +79,61 @@ WinMain (
     WIN32_FIND_DATAA    wfda;
 
     /* 复制指定的配置文件 */
-    sprintf(exec, QST_PATH_CONFIG "%s", conf_name);
-    find = FindFirstFileA(exec, &wfda);
-    if ((find == INVALID_HANDLE_VALUE) ||
-       !(wfda.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-        conf_name = DEF_CONF_NAME;
-    sprintf(exec, "cmd.exe /c copy " QST_PATH_CONFIG
-            "%s\\*.* " QST_PATH_CONFIG, conf_name);
-    if (find != INVALID_HANDLE_VALUE)
-        FindClose(find);
-    misc_call_exe(exec, TRUE, TRUE);
+    misc_call_exe("cmd.exe /c copy " QST_PATH_CONFIG
+                  "base\\*.* " QST_PATH_CONFIG, TRUE, TRUE);
+    if (conf_name != NULL) {
+        sprintf(exec, QST_PATH_CONFIG "%s", conf_name);
+        find = FindFirstFileA(exec, &wfda);
+        if (find != INVALID_HANDLE_VALUE) {
+            if (wfda.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                sprintf(exec, "cmd.exe /c copy " QST_PATH_CONFIG
+                        "%s\\*.* " QST_PATH_CONFIG, conf_name);
+                misc_call_exe(exec, TRUE, TRUE);
+            }
+            FindClose(find);
+        }
+    }
+
+    uint_t  scn_w = GetSystemMetrics(SM_CXSCREEN);
+    uint_t  scn_h = GetSystemMetrics(SM_CYSCREEN);
 
     /* 复制指定的窗口布局 */
-    sprintf(exec, QST_PATH_WINDOW "%s", conf_name);
-    find = FindFirstFileA(exec, &wfda);
-    if ((find == INVALID_HANDLE_VALUE) ||
-       !(wfda.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-        conf_name = DEF_CONF_NAME;
     sprintf(exec, "cmd.exe /c copy " QST_PATH_WINDOW
-            "%s\\%ux%u\\*.* " QST_PATH_WINDOW, conf_name,
-                    GetSystemMetrics(SM_CXSCREEN),
-                    GetSystemMetrics(SM_CYSCREEN));
-    if (find != INVALID_HANDLE_VALUE)
-        FindClose(find);
+            "base\\%ux%u\\*.* " QST_PATH_WINDOW, scn_w, scn_h);
     misc_call_exe(exec, TRUE, TRUE);
+    if (conf_name != NULL) {
+        sprintf(exec, QST_PATH_WINDOW "%s", conf_name);
+        find = FindFirstFileA(exec, &wfda);
+        if (find != INVALID_HANDLE_VALUE) {
+            if (wfda.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                sprintf(exec, "cmd.exe /c copy " QST_PATH_WINDOW
+                        "%s\\%ux%u\\*.* " QST_PATH_WINDOW, conf_name,
+                                    scn_w, scn_h);
+                misc_call_exe(exec, TRUE, TRUE);
+            }
+            FindClose(find);
+        }
+    }
 
     /* 启动广播服务器程序 */
     if (!misc_call_exe("QstServ.exe", FALSE, FALSE))
         return (QST_ERROR);
 
     /* 复制指定的 SciTE 配置文件 */
-    sprintf(exec, QST_PATH_EXT3RD "wscite\\%s", conf_name);
-    find = FindFirstFileA(exec, &wfda);
-    if ((find == INVALID_HANDLE_VALUE) ||
-       !(wfda.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-        conf_name = DEF_CONF_NAME;
-    sprintf(exec, "cmd.exe /c copy " QST_PATH_EXT3RD "wscite\\"
-            "%s\\*.* " QST_PATH_EXT3RD "wscite\\", conf_name);
-    if (find != INVALID_HANDLE_VALUE)
-        FindClose(find);
-    misc_call_exe(exec, FALSE, TRUE);
+    misc_call_exe("cmd.exe /c copy " QST_PATH_EXT3RD "wscite\\"
+                  "base\\*.* " QST_PATH_EXT3RD "wscite\\", TRUE, TRUE);
+    if (conf_name != NULL) {
+        sprintf(exec, QST_PATH_EXT3RD "wscite\\%s", conf_name);
+        find = FindFirstFileA(exec, &wfda);
+        if (find != INVALID_HANDLE_VALUE) {
+            if (wfda.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+                sprintf(exec, "cmd.exe /c copy " QST_PATH_EXT3RD "wscite\\"
+                        "%s\\*.* " QST_PATH_EXT3RD "wscite\\", conf_name);
+                misc_call_exe(exec, FALSE, TRUE);
+            }
+            FindClose(find);
+        }
+    }
 
     sINIu*  ini;
     ansi_t* str;
