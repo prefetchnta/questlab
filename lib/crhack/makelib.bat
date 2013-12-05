1f98d6595b7afc63f198cced83034d82d6e962b9
prefetchnta <crhackos@gmail.com>
2013-12-05 14:07:46 +0800

    SDK: CrHack 导入库重命名

--- lib/crhack/makelib.bat
+++ lib/crhack/makelib.bat
@@ -2,58 +2,58 @@
 rem ========================================
 :qstlibs
 if not exist QstLibs.lib goto ocr3rd
-del QstLibs_bcc.lib
-del QstLibs_msc.lib
-coff2omf QstLibs.lib QstLibs_bcc.lib
-ren QstLibs.lib QstLibs_msc.lib
+del coff\QstLibs.lib
+del omf\QstLibs.lib
+coff2omf QstLibs.lib omf\QstLibs.lib
+move QstLibs.lib coff\QstLibs.lib
 rem ========================================
 :ocr3rd
 if not exist ocr3rd.lib goto resloader
-del ocr3rd_bcc.lib
-del ocr3rd_msc.lib
-coff2omf ocr3rd.lib ocr3rd_bcc.lib
-ren ocr3rd.lib ocr3rd_msc.lib
+del coff\ocr3rd.lib
+del omf\ocr3rd.lib
+coff2omf ocr3rd.lib omf\ocr3rd.lib
+move ocr3rd.lib coff\ocr3rd.lib
 rem ========================================
 :resloader
 if not exist ResLoader.lib goto crh_blit
-del ResLoader_bcc.lib
-del ResLoader_msc.lib
-coff2omf ResLoader.lib ResLoader_bcc.lib
-ren ResLoader.lib ResLoader_msc.lib
+del coff\ResLoader.lib
+del omf\ResLoader.lib
+coff2omf ResLoader.lib omf\ResLoader.lib
+move ResLoader.lib coff\ResLoader.lib
 rem ========================================
 :crh_blit
 if not exist CrH_BLIT.lib goto crh_core
-del CrH_BLIT_bcc.lib
-del CrH_BLIT_msc.lib
-coff2omf CrH_BLIT.lib CrH_BLIT_bcc.lib
-ren CrH_BLIT.lib CrH_BLIT_msc.lib
+del coff\CrH_BLIT.lib
+del omf\CrH_BLIT.lib
+coff2omf CrH_BLIT.lib omf\CrH_BLIT.lib
+move CrH_BLIT.lib coff\CrH_BLIT.lib
 rem ========================================
 :crh_core
 if not exist CrH_CORE.lib goto crh_extz
-del CrH_CORE_bcc.lib
-del CrH_CORE_msc.lib
-coff2omf CrH_CORE.lib CrH_CORE_bcc.lib
-ren CrH_CORE.lib CrH_CORE_msc.lib
+del coff\CrH_CORE.lib
+del omf\CrH_CORE.lib
+coff2omf CrH_CORE.lib omf\CrH_CORE.lib
+move CrH_CORE.lib coff\CrH_CORE.lib
 rem ========================================
 :crh_extz
 if not exist CrH_EXTZ.lib goto crh_fmtz
-del CrH_EXTZ_bcc.lib
-del CrH_EXTZ_msc.lib
-coff2omf CrH_EXTZ.lib CrH_EXTZ_bcc.lib
-ren CrH_EXTZ.lib CrH_EXTZ_msc.lib
+del coff\CrH_EXTZ.lib
+del omf\CrH_EXTZ.lib
+coff2omf CrH_EXTZ.lib omf\CrH_EXTZ.lib
+move CrH_EXTZ.lib coff\CrH_EXTZ.lib
 rem ========================================
 :crh_fmtz
 if not exist CrH_FMTZ.lib goto crh_math
-del CrH_FMTZ_bcc.lib
-del CrH_FMTZ_msc.lib
-coff2omf CrH_FMTZ.lib CrH_FMTZ_bcc.lib
-ren CrH_FMTZ.lib CrH_FMTZ_msc.lib
+del coff\CrH_FMTZ.lib
+del omf\CrH_FMTZ.lib
+coff2omf CrH_FMTZ.lib omf\CrH_FMTZ.lib
+move CrH_FMTZ.lib coff\CrH_FMTZ.lib
 rem ========================================
 :crh_math
 if not exist CrH_MATH.lib goto bat_exit
-del CrH_MATH_bcc.lib
-del CrH_MATH_msc.lib
-coff2omf CrH_MATH.lib CrH_MATH_bcc.lib
-ren CrH_MATH.lib CrH_MATH_msc.lib
+del coff\CrH_MATH.lib
+del omf\CrH_MATH.lib
+coff2omf CrH_MATH.lib omf\CrH_MATH.lib
+move CrH_MATH.lib coff\CrH_MATH.lib
 rem ========================================
 :bat_exit
