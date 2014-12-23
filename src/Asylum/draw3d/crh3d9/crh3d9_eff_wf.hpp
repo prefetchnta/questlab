5641ed5e4304123b5c037d350a4a8cae9f888f44
prefetchnta <crhackos@gmail.com>
2014-12-23 16:25:53 +0800

    Asylum: 添加 WAVEFRONT 的效果实现

---
+++ src/Asylum/draw3d/crh3d9/crh3d9_eff_wf.hpp
@@ -0,0 +1,15 @@
+/*
+***************************************
+*   Asylum3D @ 2014-12-23
+***************************************
+*/
+
+#ifndef __CRH3D9_EFF_WF_HPP__
+#define __CRH3D9_EFF_WF_HPP__
+
+/***************/
+/* Factory API */
+/***************/
+CR_API effect_i* create_crh3d9_eff_wf_fixed (const vec4d_t* ambient, D3DLIGHT9** light, size_t count);
+
+#endif  /* __CRH3D9_EFF_WF_HPP__ */
