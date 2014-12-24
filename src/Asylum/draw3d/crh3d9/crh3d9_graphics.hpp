06dad82428d6e7f68a5d565b620baa0b66f7c98e
prefetchnta <crhackos@gmail.com>
2014-12-24 11:03:57 +0800

    Asylum: 所有工厂接口汇集到一个头文件里声明

---
+++ src/Asylum/draw3d/crh3d9/crh3d9_graphics.hpp
@@ -0,0 +1,16 @@
+/*
+***************************************
+*   Asylum3D @ 2014-12-23
+***************************************
+*/
+
+#ifndef __CRH3D9_GRAPHICS_HPP__
+#define __CRH3D9_GRAPHICS_HPP__
+
+/******************/
+/* Effect Factory */
+/******************/
+CR_API asy::IEffect* create_crh3d9_eff_wf_fixed (cl32_t ambient, D3DLIGHT9** light,
+        BOOL* on_off, DWORD count, BOOL specular, const asy::crh3d9_main* main);
+
+#endif  /* __CRH3D9_GRAPHICS_HPP__ */
