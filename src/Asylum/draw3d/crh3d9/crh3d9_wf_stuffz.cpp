17b0cd63bb37d26ce8fac65e12cbc2e0cdd3f722
prefetchnta <crhackos@gmail.com>
2014-12-25 10:16:09 +0800

    Asylum: 删除多余的代码

--- src/Asylum/draw3d/crh3d9/crh3d9_wf_stuffz.cpp
+++ src/Asylum/draw3d/crh3d9/crh3d9_wf_stuffz.cpp
@@ -18,13 +18,7 @@ namespace asy {
 /*******************************/
 class crh3d9_attr_wf_fixed : public IAttrib
 {
-private:
-#if !defined(ASY_USE_FIXED_3D)
-    crh3d9_texr*    m_bump;
-    crh3d9_texr*    m_map_ka;
-    crh3d9_texr*    m_map_ks;
-    crh3d9_texr*    m_map_ns;
-#endif
+protected:
     D3DMATERIAL9        m_mtl;
     crh3d9_texr*        m_map_kd;
     LPDIRECT3DDEVICE9   m_device;
@@ -42,8 +36,8 @@ public:
     }
 
 public:
-    /* ================================================================== */
-    bool init (const sWAVEFRONT_M* mtl, const map_acs<crh3d9_texr>* texpool)
+    /* ===================================================================== */
+    bool init_ff (const sWAVEFRONT_M* mtl, const map_acs<crh3d9_texr>* texpool)
     {
         if (mtl->map_kd != NULL) {
             m_map_kd = texpool->get(mtl->map_kd);
@@ -69,52 +63,12 @@ public:
         m_mtl.Specular.r = mtl->ks.x;
         m_mtl.Specular.g = mtl->ks.y;
         m_mtl.Specular.b = mtl->ks.z;
-#if defined(ASY_USE_FIXED_3D)
         m_mtl.Specular.a = 0.0f;
         m_mtl.Emissive.r = 0.0f;
         m_mtl.Emissive.g = 0.0f;
         m_mtl.Emissive.b = 0.0f;
         m_mtl.Emissive.a = 0.0f;
         m_mtl.Power = mtl->ns;
-#else
-        m_mtl.Specular.a = mtl->ns;
-        m_mtl.Emissive.r = mtl->tf.x;
-        m_mtl.Emissive.g = mtl->tf.y;
-        m_mtl.Emissive.b = mtl->tf.z;
-        m_mtl.Emissive.a = 1.0f;
-        if (mtl->map_ka != NULL) {
-            m_map_ka = texpool->get(mtl->map_ka);
-            if (m_map_ka == NULL)
-                return (false);
-        }
-        else {
-            m_map_ka = NULL;
-        }
-        if (mtl->map_ks != NULL) {
-            m_map_ks = texpool->get(mtl->map_ks);
-            if (m_map_ks == NULL)
-                return (false);
-        }
-        else {
-            m_map_ks = NULL;
-        }
-        if (mtl->map_ns != NULL) {
-            m_map_ns = texpool->get(mtl->map_ns);
-            if (m_map_ns == NULL)
-                return (false);
-        }
-        else {
-            m_map_ns = NULL;
-        }
-        if (mtl->bump != NULL) {
-            m_bump = texpool->get(mtl->bump);
-            if (m_bump == NULL)
-                return (false);
-        }
-        else {
-            m_bump = NULL;
-        }
-#endif
         return (true);
     }
 
