ae9de10b56aa49682d6f556824fd6f44e532ea39
prefetchnta <crhackos@gmail.com>
2014-12-26 17:25:06 +0800

    CrH_GFX3: 使用更精确的包尾球计算方法

--- src/CrH_GFX3/bound.c
+++ src/CrH_GFX3/bound.c
@@ -137,25 +137,112 @@ bound_get_ball (
   __CR_IN__ leng_t          bpv
     )
 {
-    fp32_t      radius;
-    fp32_t      xx, yy, zz;
-    vec3d_t     min, max;
+    leng_t          idx;
+    const vec3d_t*  ptr;
+    /* -------------- */
+    fp32_t  min_x, min_y, min_z;
+    fp32_t  max_x, max_y, max_z;
+    leng_t  idx_min_x, idx_min_y, idx_min_z;
+    leng_t  idx_max_x, idx_max_y, idx_max_z;
 
-    bound_min_max(&min, &max, pos, count, bpv);
+    ptr = pos;
+    min_x = max_x = ptr->x;
+    min_y = max_y = ptr->y;
+    min_z = max_z = ptr->z;
+    idx_min_x = idx_min_y = idx_min_z = 0;
+    idx_max_x = idx_max_y = idx_max_z = 0;
+    ptr = (vec3d_t*)((byte_t*)ptr + bpv);
+    for (idx = 1; idx < count; idx++) {
+        if (ptr->x < min_x) { min_x = ptr->x; idx_min_x = idx; }
+        if (ptr->y < min_y) { min_y = ptr->y; idx_min_y = idx; }
+        if (ptr->z < min_z) { min_z = ptr->z; idx_min_z = idx; }
+        if (ptr->x > max_x) { max_x = ptr->x; idx_max_x = idx; }
+        if (ptr->y > max_y) { max_y = ptr->y; idx_max_y = idx; }
+        if (ptr->z > max_z) { max_z = ptr->z; idx_max_z = idx; }
+        ptr = (vec3d_t*)((byte_t*)ptr + bpv);
+    }
 
-    ball->center.x = (min.x + max.x) / 2.0f;
-    ball->center.y = (min.y + max.y) / 2.0f;
-    ball->center.z = (min.z + max.z) / 2.0f;
+    idx = idx_max_x * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    max_x = ptr->x;
+    max_y = ptr->y;
+    max_z = ptr->z;
+    idx = idx_min_x * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    max_x -= ptr->x;
+    max_y -= ptr->y;
+    max_z -= ptr->z;
+    min_x = max_x * max_x + max_y * max_y + max_z * max_z;
 
-    ball->radius = -1.0f;
-    for (; count != 0; count--) {
-        xx = ball->center.x - pos->x;
-        yy = ball->center.y - pos->y;
-        zz = ball->center.z - pos->z;
-        radius = FSQRT(xx * xx + yy * yy + zz * zz);
-        if (radius > ball->radius)
-            ball->radius = radius;
+    idx = idx_max_y * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    max_x = ptr->x;
+    max_y = ptr->y;
+    max_z = ptr->z;
+    idx = idx_min_y * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    max_x -= ptr->x;
+    max_y -= ptr->y;
+    max_z -= ptr->z;
+    min_y = max_x * max_x + max_y * max_y + max_z * max_z;
+
+    idx = idx_max_z * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    max_x = ptr->x;
+    max_y = ptr->y;
+    max_z = ptr->z;
+    idx = idx_min_z * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    max_x -= ptr->x;
+    max_y -= ptr->y;
+    max_z -= ptr->z;
+    min_z = max_x * max_x + max_y * max_y + max_z * max_z;
+
+    if (zz > xx && zz > yy) {
+        ball->radius = min_z;
+        idx_max_x = idx_max_z;
+        idx_min_x = idx_min_z;
+    }
+    else
+    if (yy > xx && yy > zz) {
+        ball->radius = min_y;
+        idx_max_x = idx_max_y;
+        idx_min_x = idx_min_y;
+    }
+    else {
+        ball->radius = min_x;
+    }
+    idx = idx_max_x * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    ball->center.x = ptr->x;
+    ball->center.y = ptr->y;
+    ball->center.z = ptr->z;
+    idx = idx_min_x * bpv;
+    ptr = (vec3d_t*)((byte_t*)pos + idx);
+    ball->center.x += ptr->x;
+    ball->center.y += ptr->y;
+    ball->center.z += ptr->z;
+    ball->center.x *= 0.5f;
+    ball->center.y *= 0.5f;
+    ball->center.z *= 0.5f;
+    ball->radius = FSQRT(ball->radius) * 0.5f;
+    min_y = ball->radius * ball->radius;
+
+    for (idx = 0; idx < count; idx++) {
+        max_x = ball->center.x - pos->x;
+        max_y = ball->center.y - pos->y;
+        max_z = ball->center.z - pos->z;
         pos = (vec3d_t*)((byte_t*)pos + bpv);
+        min_x = max_x * max_x + max_y * max_y + max_z * max_z;
+        if (min_x > min_y) {
+            min_x = FSQRT(min_x);
+            min_z = (min_x + ball->radius) * 0.5f;
+            min_x = (min_z - ball->radius) / min_x;
+            ball->radius = min_z;
+            ball->center.x += max_x * min_x;
+            ball->center.y += max_y * min_x;
+            ball->center.z += max_z * min_x;
+        }
     }
 }
 
