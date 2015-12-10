8150631af80256444ecc3c600e5c616ac0120c65
prefetchnta <crhackos@gmail.com>
2015-12-10 13:46:42 +0800

    SDK: FMOD 升级到 V4.44.59

--- src/CrH_FMTZ/fmod/fmod.h
+++ src/CrH_FMTZ/fmod/fmod.h
@@ -15,7 +15,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00044456
+#define FMOD_VERSION    0x00044459
 
 /*
     Compiler specific settings.
