417edf22cb22680ea137e47ee1f57d39af592d1e
prefetchnta <crhackos@gmail.com>
2017-01-09 17:10:44 +0800

    CrHackSet: 完成 ZXing 识别基本流程

---
+++ src/CrHackSet/zxing_decode.c
@@ -0,0 +1,227 @@
+
+#include "../QstLibs/QstLibs.h"
+
+#include <string>
+#include <zxing/common/Counted.h>
+#include <zxing/Binarizer.h>
+#include <zxing/MultiFormatReader.h>
+#include <zxing/Result.h>
+#include <zxing/ReaderException.h>
+#include <zxing/common/GlobalHistogramBinarizer.h>
+#include <zxing/common/HybridBinarizer.h>
+#include <exception>
+#include <zxing/Exception.h>
+#include <zxing/common/IllegalArgumentException.h>
+#include <zxing/BinaryBitmap.h>
+#include <zxing/DecodeHints.h>
+#include <zxing/qrcode/QRCodeReader.h>
+#include <zxing/multi/qrcode/QRCodeMultiReader.h>
+#include <zxing/multi/ByQuadrantReader.h>
+#include <zxing/multi/MultipleBarcodeReader.h>
+#include <zxing/multi/GenericMultipleBarcodeReader.h>
+
+using namespace std;
+using namespace zxing;
+using namespace zxing::multi;
+using namespace zxing::qrcode;
+
+/*
+---------------------------------------
+    ZXing 识别单个码
+---------------------------------------
+*/
+static vector<Ref<Result> >
+decode (
+  __CR_IN__ Ref<BinaryBitmap>   image,
+  __CR_IN__ DecodeHints         hints
+    )
+{
+    Ref<Reader> reader(new MultiFormatReader);
+
+    return (vector<Ref<Result> >(1, reader->decode(image, hints)));
+}
+
+/*
+---------------------------------------
+    ZXing 识别多个码
+---------------------------------------
+*/
+static vector<Ref<Result> >
+decode_multi (
+  __CR_IN__ Ref<BinaryBitmap>   image,
+  __CR_IN__ DecodeHints         hints
+    )
+{
+    MultiFormatReader               delegate;
+    GenericMultipleBarcodeReader    reader(delegate);
+
+    return (reader.decodeMultiple(image, hints));
+}
+
+/*
+---------------------------------------
+    ZXing 识别图片
+---------------------------------------
+*/
+static sint_t
+read_image (
+  __CR_IN__ socket_t                netw,
+  __CR_IN__ Ref<LuminanceSource>    source,
+  __CR_IN__ bool                    hybrid,
+  __CR_IN__ bool                    multi,
+  __CR_IN__ DecodeHintType          type,
+  __CR_IN__ uint_t                  cpage,
+  __CR_OT__ sPNT2**                 pnts,
+  __CR_OT__ leng_t*                 count
+    )
+{
+    sPNT2                   pt;
+    sARRAY                  loc;
+    ansi_t*                 str;
+    ansi_t*                 tmp;
+    uint_t                  cnt = 0;
+    sint_t                  res = -1;
+    vector<Ref<Result> >    results;
+    string                  cell_result;
+
+    /* 创建对象 */
+    *pnts = NULL;
+    *count = 0;
+    array_initT(&loc, sPNT2);
+
+    try {
+        Ref<Binarizer>  binarizer;
+
+        /* 两种不同的二值化方法 */
+        if (hybrid)
+            binarizer = new HybridBinarizer (source);
+        else
+            binarizer = new GlobalHistogramBinarizer (source);
+
+        if (type == 0)
+            type = DecodeHints::DEFAULT_HINT;
+
+        DecodeHints hints(type);
+
+        hints.setTryHarder(true);
+
+        Ref<BinaryBitmap>   binary(new BinaryBitmap (binarizer));
+
+        /* 搜索多个码或单个码 */
+        if (multi)
+            results = decode_multi(binary, hints);
+        else
+            results = decode(binary, hints);
+        res = 0;
+    }
+    catch (const ReaderException& e) {
+        cell_result = "zxing::ReaderException: " + string(e.what());
+        res = -2;
+    }
+    catch (const zxing::IllegalArgumentException& e) {
+        cell_result = "zxing::IllegalArgumentException: " + string(e.what());
+        res = -3;
+    }
+    catch (const zxing::Exception& e) {
+        cell_result = "zxing::Exception: " + string(e.what());
+        res = -4;
+    }
+    catch (const std::exception& e) {
+        cell_result = "std::exception: " + string(e.what());
+        res = -5;
+    }
+
+    /* 输出结果 */
+    if (netw != NULL)
+        cmd_shl_send(netw, "txt:clear 0 0");
+    if (res != 0) {
+        if (netw != NULL) {
+            str = str_esc_makeU(cell_result.c_str());
+            if (str != NULL) {
+                tmp = str_fmtA("info::main=\"0> %s\"", str);
+                mem_free(str);
+                if (tmp != NULL) {
+                    cmd_ini_send(netw, tmp);
+                    mem_free(tmp);
+                }
+            }
+        }
+        return (res);
+    }
+    for (size_t ii = 0; ii < results.size(); ii++) {
+        cnt += 1;
+        if (netw != NULL) {
+            str = str_fmtA("%s:%s", BarcodeFormat::barcodeFormatNames[
+                                    results[ii]->getBarcodeFormat()],
+                                    results[ii]->getText()->getText());
+            if (str == NULL)
+                continue;
+            tmp = utf8_to_local(cpage, str);
+            mem_free(str);
+            if (tmp == NULL)
+                continue;
+            str = str_esc_makeU(tmp);
+            mem_free(tmp);
+            if (str == NULL)
+                continue;
+            tmp = str_fmtA("info::main=\"0> %s\"", str);
+            mem_free(str);
+            if (tmp == NULL)
+                continue;
+            cmd_ini_send(netw, tmp);
+            mem_free(tmp);
+        }
+
+        uint_t  jj = 0;
+
+        for (; jj < results[ii]->getResultPoints()->size(); jj++) {
+            pt.x = (sint_t)(results[ii]->getResultPoints()[jj]->getX());
+            pt.y = (sint_t)(results[ii]->getResultPoints()[jj]->getY());
+            array_push_growT(&loc, sPNT2, &pt);
+        }
+    }
+    *pnts  = array_get_dataT(&loc, sPNT2);
+    *count = array_get_sizeT(&loc, sPNT2);
+    return (cnt);
+}
+
+/*
+=======================================
+    ZXing 识别部分代码
+=======================================
+*/
+extern uint_t
+zxing_do_decode (
+  __CR_IN__ socket_t        netw,
+  __CR_IN__ const sIMAGE*   gray,
+  __CR_IN__ bool_t          hybrid,
+  __CR_IN__ bool_t          multi,
+  __CR_IN__ uint_t          type,
+  __CR_IN__ uint_t          cpage,
+  __CR_OT__ sPNT2**         pnts,
+  __CR_OT__ leng_t*         count
+    )
+{
+    size_t  sz;
+    byte_t  *dst, *src, *dat;
+    uint_t  ww, hh, yy, cnt;
+
+    /* 图像数据不需要行对齐 */
+    ww = gray->position.ww;
+    hh = gray->position.hh;
+    sz = ww * hh;
+    dat = (byte_t*)mem_malloc(sz);
+    if (dat == NULL)
+        return (0);
+    dst = dat;
+    src = gray->data;
+    for (yy = 0; yy < hh; yy++) {
+        mem_cpy(dst, src, ww);
+        dst += ww;
+        src += gray->bpl;
+    }
+    cnt = read_image(netw, source, !!hybrid, !!multi,
+                     type, cpage, pnts, count);
+    mem_free(dat);
+    return (cnt);
+}
