fba4d8a55f47f1142aa6e619f9b35164a8a94a2b
prefetchnta <crhackos@gmail.com>
2017-01-17 15:41:45 +0800

    CrHack: 更新 cJSON 头文件

--- inc/crhack/util/cjson/cJSON.h
+++ inc/crhack/util/cjson/cJSON.h
@@ -28,6 +28,14 @@ extern "C"
 {
 #endif
 
+/* project version */
+#define CJSON_VERSION_MAJOR 1
+#define CJSON_VERSION_MINOR 2
+#define CJSON_VERSION_PATCH 0
+
+/* returns the version of cJSON as a string */
+extern const char* cJSON_Version(void);
+
 #include <stddef.h>
 
 /* cJSON Types: */
