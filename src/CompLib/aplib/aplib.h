ed50c4090c856680d47ab1df359a56514ae8d185
prefetchnta <crhackos@gmail.com>
2017-01-19 17:07:19 +0800

    CompLib: aPLib 升级到 V1.1.1

--- src/CompLib/aplib/aplib.h
+++ src/CompLib/aplib/aplib.h
@@ -1,7 +1,9 @@
 /*
  * aPLib compression library  -  the smaller the better :)
  *
- * Copyright (c) 1998-2009 by Joergen Ibsen / Jibz
+ * COFF format header file
+ *
+ * Copyright (c) 1998-2014 Joergen Ibsen
  * All Rights Reserved
  *
  * http://www.ibsensoftware.com/
@@ -15,46 +17,46 @@ extern "C" {
 #endif
 
 #ifndef APLIB_ERROR
-# define APLIB_ERROR (-1)
+# define APLIB_ERROR ((unsigned int) (-1))
 #endif
 
-unsigned int aP_pack(const void *source,
-                     void *destination,
-                     unsigned int length,
-                     void *workmem,
-                     int (*callback)(unsigned int, unsigned int, unsigned int, void *),
-                     void *cbparam);
+unsigned int __cdecl aP_pack(const void *source,
+                             void *destination,
+                             unsigned int length,
+                             void *workmem,
+                             int (__cdecl *callback)(unsigned int, unsigned int, unsigned int, void *),
+                             void *cbparam);
 
-unsigned int aP_workmem_size(unsigned int inputsize);
+unsigned int __cdecl aP_workmem_size(unsigned int inputsize);
 
-unsigned int aP_max_packed_size(unsigned int inputsize);
+unsigned int __cdecl aP_max_packed_size(unsigned int inputsize);
 
-unsigned int aP_depack_asm(const void *source, void *destination);
+unsigned int __cdecl aP_depack_asm(const void *source, void *destination);
 
-unsigned int aP_depack_asm_fast(const void *source, void *destination);
+unsigned int __cdecl aP_depack_asm_fast(const void *source, void *destination);
 
-unsigned int aP_depack_asm_safe(const void *source,
-                                unsigned int srclen,
-                                void *destination,
-                                unsigned int dstlen);
+unsigned int __cdecl aP_depack_asm_safe(const void *source,
+                                        unsigned int srclen,
+                                        void *destination,
+                                        unsigned int dstlen);
 
-unsigned int aP_crc32(const void *source, unsigned int length);
+unsigned int __cdecl aP_crc32(const void *source, unsigned int length);
 
-unsigned int aPsafe_pack(const void *source,
-                         void *destination,
-                         unsigned int length,
-                         void *workmem,
-                         int (*callback)(unsigned int, unsigned int, unsigned int, void *),
-                         void *cbparam);
+unsigned int __cdecl aPsafe_pack(const void *source,
+                                 void *destination,
+                                 unsigned int length,
+                                 void *workmem,
+                                 int (__cdecl *callback)(unsigned int, unsigned int, unsigned int, void *),
+                                 void *cbparam);
 
-unsigned int aPsafe_check(const void *source);
+unsigned int __cdecl aPsafe_check(const void *source);
 
-unsigned int aPsafe_get_orig_size(const void *source);
+unsigned int __cdecl aPsafe_get_orig_size(const void *source);
 
-unsigned int aPsafe_depack(const void *source,
-                           unsigned int srclen,
-                           void *destination,
-                           unsigned int dstlen);
+unsigned int __cdecl aPsafe_depack(const void *source,
+                                   unsigned int srclen,
+                                   void *destination,
+                                   unsigned int dstlen);
 
 #ifdef __cplusplus
 } /* extern "C" */
