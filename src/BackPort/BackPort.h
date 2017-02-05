16e2b69d689aabfd578782a255a0f396eaeb324c
prefetchnta <crhackos@gmail.com>
2017-02-05 16:15:11 +0800

    BackPort: 函数映射表准备完毕

--- src/BackPort/BackPort.h
+++ src/BackPort/BackPort.h
@@ -23,7 +23,23 @@
 #ifndef __QL_BACKPORT_H__
 #define __QL_BACKPORT_H__
 
-#include "crhack.h"
+#include "defs.h"
+
+#define DLL_PROCESS_ATTACH  1
+#define DLL_THREAD_ATTACH   2
+#define DLL_THREAD_DETACH   3
+#define DLL_PROCESS_DETACH  0
+
+/* 函数列表单元 */
+typedef struct
+{
+        void_t*         addr;
+        const ansi_t*   name;
+
+} sBP_FUNC;
+
+void_t* func_load (sBP_FUNC *func, const ansi_t *dll);
+void_t  func_free (void_t *handle);
 
 /* 简化函数定义 */
 
--- src/BackPort/kernel33.c
+++ src/BackPort/kernel33.c
@@ -1,30 +1,103 @@
 
 #include "BackPort.h"
 
-#if defined(_CR_BUILD_DLL_)
+/* 函数重映射表 */
+static sBP_FUNC s_remap[] =
+{
+    { NULL, "CloseHandle" },
+    { NULL, "CreateFileW" },
+    { NULL, "DecodePointer" },
+    { NULL, "DeleteCriticalSection" },
+    { NULL, "EncodePointer" },
+    { NULL, "EnterCriticalSection" },
+    { NULL, "ExitProcess" },
+    { NULL, "FlsAlloc" },
+    { NULL, "FlsFree" },
+    { NULL, "FlsGetValue" },
+    { NULL, "FlsSetValue" },
+    { NULL, "FlushFileBuffers" },
+    { NULL, "FreeEnvironmentStringsW" },
+    { NULL, "GetACP" },
+    { NULL, "GetCommandLineA" },
+    { NULL, "GetConsoleCP" },
+    { NULL, "GetConsoleMode" },
+    { NULL, "GetCPInfo" },
+    { NULL, "GetCurrentProcess" },
+    { NULL, "GetCurrentThreadId" },
+    { NULL, "GetEnvironmentStringsW" },
+    { NULL, "GetFileType" },
+    { NULL, "GetLastError" },
+    { NULL, "GetModuleFileNameA" },
+    { NULL, "GetModuleFileNameW" },
+    { NULL, "GetModuleHandleExW" },
+    { NULL, "GetModuleHandleW" },
+    { NULL, "GetOEMCP" },
+    { NULL, "GetProcAddress" },
+    { NULL, "GetProcessHeap" },
+    { NULL, "GetStartupInfoW" },
+    { NULL, "GetStdHandle" },
+    { NULL, "GetStringTypeW" },
+    { NULL, "GetSystemTimeAsFileTime" },
+    { NULL, "GetTickCount64" },
+    { NULL, "HeapAlloc" },
+    { NULL, "HeapFree" },
+    { NULL, "HeapReAlloc" },
+    { NULL, "HeapSize" },
+    { NULL, "InitializeCriticalSectionAndSpinCount" },
+    { NULL, "InitOnceExecuteOnce" },
+    { NULL, "InterlockedDecrement" },
+    { NULL, "InterlockedIncrement" },
+    { NULL, "IsDebuggerPresent" },
+    { NULL, "IsProcessorFeaturePresent" },
+    { NULL, "IsValidCodePage" },
+    { NULL, "LCMapStringEx" },
+    { NULL, "LeaveCriticalSection" },
+    { NULL, "LoadLibraryExW" },
+    { NULL, "LoadLibraryW" },
+    { NULL, "MultiByteToWideChar" },
+    { NULL, "OutputDebugStringW" },
+    { NULL, "QueryPerformanceCounter" },
+    { NULL, "RaiseException" },
+    { NULL, "RtlUnwind" },
+    { NULL, "SetFilePointerEx" },
+    { NULL, "SetLastError" },
+    { NULL, "SetStdHandle" },
+    { NULL, "SetUnhandledExceptionFilter" },
+    { NULL, "Sleep" },
+    { NULL, "TerminateProcess" },
+    { NULL, "UnhandledExceptionFilter" },
+    { NULL, "WideCharToMultiByte" },
+    { NULL, "WriteConsoleW" },
+    { NULL, "WriteFile" },
+};
+
+/* DLL 句柄 */
+static void_t*  s_kernel32 = NULL;
+
 /*
 =======================================
     DLL 入口点
 =======================================
 */
-BOOL WINAPI
+int __stdcall
 DllMain (
-  __CR_IN__ HANDLE  hinst,
-  __CR_IN__ DWORD   reason,
-  __CR_UU__ LPVOID  reserved
+  __CR_IN__ void*   hinst,
+  __CR_IN__ int     reason,
+  __CR_UU__ void*   reserved
     )
 {
     switch (reason)
     {
         case DLL_PROCESS_ATTACH:
+            s_kernel32 = func_load(s_remap, "KERNEL32.dll");
             break;
 
         case DLL_PROCESS_DETACH:
+            if (s_kernel32 != NULL)
+                func_free(s_kernel32);
             break;
     }
     CR_NOUSE(hinst);
     CR_NOUSE(reserved);
     return (TRUE);
 }
-
-#endif  /* _CR_BUILD_DLL_ */
