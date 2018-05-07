7a9fa704407db8a95e87e9796970d57ac3be086d
prefetchnta <crhackos@gmail.com>
2018-05-07 13:26:51 +0800

    TOOLS: 补上漏掉的百分号

--- bin/ssl32.bat
+++ bin/ssl32.bat
@@ -1,5 +1,5 @@
 @echo off
-set HOME=~dp0crypto
-set OPENSSL_ENGINES=~dp0crypto
-set OPENSSL_CONF=~dp0crypto\openssl.cnf
+set HOME=%~dp0crypto
+set OPENSSL_ENGINES=%~dp0crypto
+set OPENSSL_CONF=%~dp0crypto\openssl.cnf
 %~dp0openssl.exe
