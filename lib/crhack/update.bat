ff09b9e8ab4f26fc2cd026af75ed57514f3092b6
prefetchnta <crhackos@gmail.com>
2019-03-06 18:17:57 +0800

    CrHack: 更新最新二进制

--- lib/crhack/update.bat
+++ lib/crhack/update.bat
@@ -7,6 +7,7 @@ copy /Y ..\..\..\FinalBIN\CRHACK\vc2010_win32_x86\CrH_MATH.lib
 call makelib.bat
 cd ..\..\bin
 copy /Y ..\..\FinalBIN\CRHACK\vc2010_win32_x86\CrH_*.dll
+copy /Y ..\..\FinalBIN\CRHACK\vc2010_win32_x86\GFX?_*.dll
 copy /Y ..\..\FinalBIN\CRHACK\vc2010_win32_x86\z*.dll plugin
 copy /Y ..\..\FinalBIN\CRHACK\vc2003_win32_x86\zSYN.dll plugin
 pause
