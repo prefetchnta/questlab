f88d8116f8ee8b1bf2bd7d9b2fbe66ea79b5e792
prefetchnta <crhackos@gmail.com>
2022-04-06 13:26:26 +0800

    httpcopy: 支持 XML 格式的文件列表

--- src/ToolBox/httpcopy.cpp
+++ src/ToolBox/httpcopy.cpp
@@ -22,6 +22,191 @@
 
 #include "../QstLibs/QstLibs.h"
 
+/* 简化代码宏 */
+#define DO_FILE_NODE(_path, _root) \
+\
+path_buildA(_path); \
+str = str_dupA(_path); \
+if (str == NULL) { \
+    printf("str_dupA() failure\n"); \
+    return (QST_ERROR); \
+} \
+path_extractA(str, _path); \
+len = str_lenA(str); \
+if (len != 0 && _path[len] != 0) { \
+    url = str_fmtA("@move /y \"%s\" \"%s\"\r\n", &_path[len], str); \
+    if (url == NULL) { \
+        fprintf(fpb, "@move /y \"%s\" \"%s\"\r\n", &_path[len], str); \
+    } \
+    else { \
+        len = str_lenA(url); \
+        for (leng_t jj = 0; jj < len; jj++) { \
+            if (url[jj] == '%') { \
+                fputc('%', fpb); \
+                fputc('%', fpb); \
+            } \
+            else { \
+                fputc(url[jj], fpb); \
+            } \
+        } \
+        mem_free(url); \
+    } \
+    fflush(fpb); \
+} \
+mem_free(str); \
+len = str_lenA(_path); \
+for (leng_t ii = 0; ii < len; ii++) { \
+    if (_path[ii] == '\\') \
+        _path[ii] = '/'; \
+} \
+url = url_encode(_path, FALSE); \
+if (url == NULL) { \
+    printf("url_encode() failure\n"); \
+    return (QST_ERROR); \
+} \
+str = str_fmtA("%s%s", _root, url); \
+mem_free(url); \
+if (str == NULL) { \
+    printf("str_fmtA() failure\n"); \
+    return (QST_ERROR); \
+} \
+fprintf(fpu, "%s\r\n", str); \
+fflush(fpu); \
+mem_free(str)
+
+/*
+---------------------------------------
+    XML 文件处理
+---------------------------------------
+*/
+static int
+do_xml_file (
+  __CR_IN__ ansi_t*         str,
+  __CR_IN__ const ansi_t*   name,
+  __CR_IN__ const ansi_t*   root
+    )
+{
+    FILE*   fpb;
+    FILE*   fpu;
+    FILE*   fph;
+    sXMLu*  xml;
+    int64u  tot;
+    leng_t  len;
+    ansi_t* pth;
+    ansi_t* md5;
+    ansi_t* crc;
+    ansi_t* bat;
+    ansi_t* url;
+
+    /* XML 标签解析 */
+    xml = xml_parseU(str);
+    mem_free(str);
+    if (xml == NULL) {
+        printf("xml_parseU() failure\n");
+        return (QST_ERROR);
+    }
+
+    /* 合成输出文件 */
+    bat = str_fmtA("%s.bat", name);
+    url = str_fmtA("%s.lst", name);
+    if (bat == NULL || url == NULL) {
+        printf("str_fmtA() failure\n");
+        return (QST_ERROR);
+    }
+    fpb = fopen(bat, "wb");
+    fpu = fopen(url, "wb");
+    mem_free(bat);
+    mem_free(url);
+    if (fpb == NULL || fpu == NULL) {
+        printf("fopen() failure\n");
+        return (QST_ERROR);
+    }
+
+    /* 哈希输出文件 */
+    fph = fopen("HaSHer.txt", "wb");
+    if (fph == NULL) {
+        printf("fopen() failure\n");
+        return (QST_ERROR);
+    }
+    tot = 0;
+    pth = NULL;
+    md5 = crc = NULL;
+
+    /* 逐个标签处理 */
+    for (leng_t idx = 0; idx < xml->count; idx++)
+    {
+        sXNODEu*    node = &xml->nodes[idx];
+
+        /* 文件路径 */
+        if (pth == NULL) {
+            if (str_cmpIA(node->name, "file") != 0)
+                continue;
+            pth = xml_attr_stringU("name", node);
+            if (pth == NULL) {
+                printf("xml_attr_stringU() failure\n");
+                return (QST_ERROR);
+            }
+        }
+        else {
+            if (str_cmpIA(node->name, "size") == 0)
+            {
+                /* 统计总大小 */
+                tot += str2int64A(node->node);
+            }
+            else
+            if (str_cmpIA(node->name, "md5") == 0)
+            {
+                /* 文件 MD5 */
+                md5 = xml_node_stringU(node);
+                if (md5 == NULL) {
+                    printf("xml_node_stringU() failure\n");
+                    return (QST_ERROR);
+                }
+                str_uprA(str_trimA(md5));
+            }
+            else
+            if (str_cmpIA(node->name, "crc32") == 0)
+            {
+                /* 文件 CRC32 */
+                crc = xml_node_stringU(node);
+                if (crc == NULL) {
+                    printf("xml_node_stringU() failure\n");
+                    return (QST_ERROR);
+                }
+                str_uprA(str_trimA(crc));
+            }
+            else
+            if (str_cmpIA(node->name, "/file") == 0)
+            {
+                /* 输出哈希列表项 */
+                fprintf(fph, "%s + %s: %s\r\n", md5, crc, pth);
+                fflush(fph);
+                SAFE_FREE(md5);
+                SAFE_FREE(crc);
+
+                /* 创建下级目录 */
+                /* 输出文件搬移脚本 */
+                DO_FILE_NODE(pth, root);
+
+                /* 下一个节点 */
+                mem_free(pth);
+                pth = NULL;
+            }
+        }
+    }
+    if (tot >= 1024 * 1024 * 1024)
+        printf("total size: %.2f GB\n", (double)tot / 1024 / 1024 / 1024);
+    else if (tot >= 1024 * 1024)
+        printf("total size: %.2f MB\n", (double)tot / 1024 / 1024);
+    else
+        printf("total size: %.2f KB\n", (double)tot / 1024);
+    xml_closeU(xml);
+    fclose(fph);
+    fclose(fpb);
+    fclose(fpu);
+    return (QST_OKAY);
+}
+
 /*
 =======================================
     主程序
@@ -60,6 +245,12 @@ int main (int argc, char *argv[])
         printf("file_load_as_strA() failure\n");
         return (QST_ERROR);
     }
+
+    /* XML 文件另外处理 */
+    if (filext_checkA(argv[1], ".xml"))
+        return (do_xml_file(str, argv[1], argv[2]));
+
+    /* INI 行解析 */
     ini = ini_parseU(str);
     mem_free(str);
     if (ini == NULL) {
@@ -94,60 +285,8 @@ int main (int argc, char *argv[])
             continue;
 
         /* 创建下级目录 */
-        path_buildA(line);
-
         /* 输出文件搬移脚本 */
-        str = str_dupA(line);
-        if (str == NULL) {
-            printf("str_dupA() failure\n");
-            return (QST_ERROR);
-        }
-        path_extractA(str, line);
-        len = str_lenA(str);
-        if (len != 0 && line[len] != 0) {
-            url = str_fmtA("@move /y \"%s\" \"%s\"\r\n", &line[len], str);
-            if (url == NULL) {
-                fprintf(fpb, "@move /y \"%s\" \"%s\"\r\n", &line[len], str);
-            }
-            else {
-                len = str_lenA(url);
-                for (leng_t jj = 0; jj < len; jj++) {
-                    if (url[jj] == '%') {
-                        fputc('%', fpb);
-                        fputc('%', fpb);
-                    }
-                    else {
-                        fputc(url[jj], fpb);
-                    }
-                }
-                mem_free(url);
-            }
-            fflush(fpb);
-        }
-        mem_free(str);
-
-        /* URL 转义 */
-        len = str_lenA(line);
-        for (leng_t ii = 0; ii < len; ii++) {
-            if (line[ii] == '\\')
-                line[ii] = '/';
-        }
-        url = url_encode(line, FALSE);
-        if (url == NULL) {
-            printf("url_encode() failure\n");
-            return (QST_ERROR);
-        }
-
-        /* 合成完整 URL 串 */
-        str = str_fmtA("%s%s", argv[2], url);
-        mem_free(url);
-        if (str == NULL) {
-            printf("str_fmtA() failure\n");
-            return (QST_ERROR);
-        }
-        fprintf(fpu, "%s\r\n", str);
-        fflush(fpu);
-        mem_free(str);
+        DO_FILE_NODE(line, argv[2]);
     }
     ini_closeU(ini);
     fclose(fpb);
