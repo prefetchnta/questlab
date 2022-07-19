b0c133b21cfc729ccee3a89bfa86c40d95a3212f
prefetchnta <crhackos@gmail.com>
2022-07-19 09:47:34 +0800

    Python: 支持 7Z 和 ZIP 的测试

--- bin/script/python/rartest.py
+++ bin/script/python/rartest.py
@@ -11,28 +11,41 @@ okays = 0
 count = 0
 flist = "FAILED.TXT"
 if len(sys.argv) < 2:
-    cmd_str = "Rar.exe t "
+    zip_cmd_str = '7z.exe t "'
+    rar_cmd_str = 'Rar.exe t "'
 else:
-    cmd_str = "Rar.exe t -p%s " % sys.argv[1]
+    zip_cmd_str = '7z.exe t -p%s "' % sys.argv[1]
+    rar_cmd_str = 'Rar.exe t -p%s "' % sys.argv[1]
 if os.path.exists(flist):
     os.remove(flist)
+os.environ["PATH"] = "C:\\Program Files\\7-Zip;" + os.environ["PATH"]
 os.environ["PATH"] = "C:\\Program Files\\WinRAR;" + os.environ["PATH"]
 for dirpath, dirnames, filenames in os.walk("."):
     if len(filenames) == 0:
         continue
     for filename in filenames:
-        if os.path.splitext(filename)[1].lower() != ".rar":
+        ext = os.path.splitext(filename)[1].lower()
+        if ext == ".rar":
+            find_part = filename.lower()
+            if find_part.count(".part") == 1:
+                find_leng = len(find_part) - 5
+                if find_part.rfind("1.rar") != find_leng:
+                    continue
+                find_leng -= 1
+                if find_part[find_leng] != 't' and find_part[find_leng] != '0':
+                    continue
+            cmd_str = rar_cmd_str
+        elif ext == ".zip" or ext == ".7z":
+            cmd_str = zip_cmd_str
+        elif ext == ".001":
+            find_part = filename.lower()
+            if find_part.count(".7z.001") != 1:
+                continue
+            cmd_str = zip_cmd_str
+        else:
             continue
-        find_part = filename.lower()
-        if find_part.count(".part") == 1:
-            find_leng = len(find_part) - 5
-            if find_part.rfind("1.rar") != find_leng:
-                continue
-            find_leng -= 1
-            if find_part[find_leng] != 't' and find_part[find_leng] != '0':
-                continue
         full_path = dirpath + "\\" + filename
-        if os.system(cmd_str + full_path) == 0:
+        if os.system(cmd_str + full_path + '"') == 0:
             okays += 1
         else:
             fp = open(flist, "a")
