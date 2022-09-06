2ad48ef7e80fdb3435448f95939c001e3f4ef4ed
prefetchnta <crhackos@gmail.com>
2022-09-06 09:41:15 +0800

    X64: 清除重复功能的程序

--- bin/down_bt.bat
+++ bin/down_bt.bat
@@ -1,6 +1,2 @@
 @echo off
-if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
-    %~dp0x64bin\aria2c.exe %3 --dir=%2 --file-allocation=none --check-integrity=true --max-concurrent-downloads=16 --max-overall-upload-limit=10K --max-upload-limit=10K --enable-dht=true %1
-) else (
-    %~dp0aria2c.exe %3 --dir=%2 --file-allocation=none --check-integrity=true --max-concurrent-downloads=16 --max-overall-upload-limit=10K --max-upload-limit=10K --enable-dht=true %1
-)
+%~dp0aria2c.exe %3 --dir=%2 --file-allocation=none --check-integrity=true --max-concurrent-downloads=16 --max-overall-upload-limit=10K --max-upload-limit=10K --enable-dht=true %1
