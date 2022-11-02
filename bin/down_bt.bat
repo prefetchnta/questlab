893a9aaec44ec4019602868a9ac1642cbd31ef43
prefetchnta <crhackos@gmail.com>
2022-11-02 13:28:10 +0800

    TOOL: 恢复 X86 和 X64 切换的批处理

--- bin/down_bt.bat
+++ bin/down_bt.bat
@@ -1,2 +1,6 @@
 @echo off
-%~dp0aria2c.exe %3 --dir=%2 --file-allocation=none --check-integrity=true --max-concurrent-downloads=16 --max-overall-upload-limit=10K --max-upload-limit=10K --enable-dht=true %1
+if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
+    %~dp0x64bin\aria2c-x64.exe %3 --dir=%2 --file-allocation=none --check-integrity=true --max-concurrent-downloads=16 --max-overall-upload-limit=10K --max-upload-limit=10K --enable-dht=true %1
+) else (
+    %~dp0aria2c.exe %3 --dir=%2 --file-allocation=none --check-integrity=true --max-concurrent-downloads=16 --max-overall-upload-limit=10K --max-upload-limit=10K --enable-dht=true %1
+)
