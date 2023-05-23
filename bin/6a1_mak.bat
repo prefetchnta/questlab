570b8ff46e6bb873aa09d720c1086a4c715b57ee
prefetchnta <crhackos@gmail.com>
2023-05-23 14:17:37 +0800

    luckyman: 增加相似度计算并调整随机数生成算法

--- bin/6a1_mak.bat
+++ bin/6a1_mak.bat
@@ -1,1 +1,1 @@
-@luckyman 88888888 23 41
+@luckyman 88888888 23 41 %QUESTLAB_DAT%\luckyman\sixa1.json
--- src/ToolBox/luckyman.cpp
+++ src/ToolBox/luckyman.cpp
@@ -21,112 +21,134 @@
  */
 
 #include "../QstLibs/QstLibs.h"
-
-#include <conio.h>
-
-uint_t  xx[7];
-uint_t  n2[10][10];
-uint_t  n3[10][10][10];
-uint_t  n4[10][10][10][10];
-uint_t  n5[10][10][10][10][10];
-uint_t  n6[10][10][10][10][10][10];
+#pragma comment (lib, "CrH_MATH.lib")
 
 /*
 ---------------------------------------
-    号码统计器
+    号码2向量 (字符串)
 ---------------------------------------
 */
-static sint_t
-luck_cmp (
-  __CR_IN__ const ansi_t*   name
+static bool_t
+luck_vec (
+  __CR_OT__ fpxx_t*         vec,
+  __CR_IN__ const ansi_t*   numb
+    )
+{
+    uint_t  idx;
+
+    if (str_lenA(numb) < 8)
+        return (FALSE);
+    for (idx = 0; idx < 6; idx++) {
+        if (!is_digitA(*numb))
+            return (FALSE);
+        vec[idx] = (fpxx_t)((int)(0x0F & *numb++));
+    }
+    numb++;
+    if (!is_digitA(*numb))
+        return (FALSE);
+    vec[idx] = (fpxx_t)((int)(0x0F & *numb));
+    return (TRUE);
+}
+
+/*
+---------------------------------------
+    号码2向量 (整数值)
+---------------------------------------
+*/
+static void_t
+luck_vec (
+  __CR_OT__ fpxx_t* vec,
+  __CR_IN__ int32u  numb
+    )
+{
+    ansi_t  str[10];
+
+    sprintf(str, "%06u+%u", numb / 10, numb % 10);
+    luck_vec(vec, str);
+}
+
+/*
+---------------------------------------
+    加载历史中奖号码
+---------------------------------------
+*/
+static fpxx_t*
+luck_his (
+  __CR_IN__ const ansi_t*   file,
+  __CR_OT__ uint_t*         count
     )
 {
     sINIu*  ini;
+    fpxx_t* dat;
 
     /* 读入文件 */
-    name = file_load_as_strA(name);
-    if (name == NULL)
-        return (-1);
-    ini = ini_parseU(name);
-    mem_free(name);
+    file = file_load_as_strA(file);
+    if (file == NULL)
+        return (NULL);
+    ini = ini_parseU(file);
+    mem_free(file);
     if (ini == NULL)
-        return (-1);
+        return (NULL);
+    dat = mem_talloc(ini->count * 7, fpxx_t);
+    if (dat == NULL) {
+        ini_closeU(ini);
+        return (NULL);
+    }
 
-    /* 解析数字 */
-    mem_zero(n2, sizeof(n2));
-    mem_zero(n3, sizeof(n3));
-    mem_zero(n4, sizeof(n4));
-    mem_zero(n5, sizeof(n5));
-    mem_zero(n6, sizeof(n6));
+    uint_t  num = 0;
+
+    /* 解析 JSON 行 */
     for (leng_t idx = 0; idx < ini->count; idx++) {
-        name = ini->lines[idx];
-        if (str_lenA(name) < 8)
-            break;
-        if (!is_digitA(name[0]) ||
-            !is_digitA(name[1]) ||
-            !is_digitA(name[2]) ||
-            !is_digitA(name[3]) ||
-            !is_digitA(name[4]) ||
-            !is_digitA(name[5]) ||
-            !is_digitA(name[7]) || name[6] != '+')
-            break;
-        xx[0] = name[0] & 0x0F;
-        xx[1] = name[1] & 0x0F;
-        xx[2] = name[2] & 0x0F;
-        xx[3] = name[3] & 0x0F;
-        xx[4] = name[4] & 0x0F;
-        xx[5] = name[5] & 0x0F;
-        xx[6] = name[7] & 0x0F;
-        for (uint_t ii = 0; ii < 6; ii++)
-            n2[xx[ii]][xx[ii+1]]++;
-        for (uint_t ii = 0; ii < 5; ii++)
-            n3[xx[ii]][xx[ii+1]][xx[ii+2]]++;
-        for (uint_t ii = 0; ii < 4; ii++)
-            n4[xx[ii]][xx[ii+1]][xx[ii+2]][xx[ii+3]]++;
-        for (uint_t ii = 0; ii < 3; ii++)
-            n5[xx[ii]][xx[ii+1]][xx[ii+2]][xx[ii+3]][xx[ii+4]]++;
-        for (uint_t ii = 0; ii < 2; ii++)
-            n6[xx[ii]][xx[ii+1]][xx[ii+2]][xx[ii+3]][xx[ii+4]][xx[ii+5]]++;
-    }
-
-    /* 输出结果 */
-    for (uint_t aa = 0; aa < 10; aa++) {
-        printf("[%u]\n", aa);
-        for (uint_t bb = 0; bb < 10; bb++) {
-            if (n2[aa][bb] == 0)
-                continue;
-            printf("  [%u]:", bb);
-            printf("%u\n", n2[aa][bb]);
-            for (uint_t cc = 0; cc < 10; cc++) {
-                if (n3[aa][bb][cc] == 0)
-                    continue;
-                printf("    [%u]:", cc);
-                printf("%u\n", n3[aa][bb][cc]);
-                for (uint_t dd = 0; dd < 10; dd++) {
-                    if (n4[aa][bb][cc][dd] == 0)
-                        continue;
-                    printf("      [%u]:", dd);
-                    printf("%u\n", n4[aa][bb][cc][dd]);
-                    for (uint_t ee = 0; ee < 10; ee++) {
-                        if (n5[aa][bb][cc][dd][ee] == 0)
-                            continue;
-                        printf("        [%u]:", ee);
-                        printf("%u\n", n5[aa][bb][cc][dd][ee]);
-                        for (uint_t ff = 0; ff < 10; ff++) {
-                            if (n6[aa][bb][cc][dd][ee][ff] == 0)
-                                continue;
-                            printf("          [%u]:", ff);
-                            printf("%u\n", n6[aa][bb][cc][dd][ee][ff]);
-                        }
-                    }
-                }
-            }
+        file = str_strA(ini->lines[idx], "\", \"number\":\"");
+        if (file == NULL)
+            continue;
+        if (!luck_vec(&dat[num * 7], file + 13)) {
+            mem_free(dat);
+            ini_closeU(ini);
+            return (NULL);
         }
+        num++;
     }
 
-    /* 释放内存 */
+    /* 释放返回 */
+    *count = num;
     ini_closeU(ini);
-    return (0);
+    return (dat);
+}
+
+/*
+---------------------------------------
+    计算号码相似度
+---------------------------------------
+*/
+static void_t
+luck_sim (
+  __CR_OT__ fpxx_t*         simx,
+  __CR_IN__ int32u          numb,
+  __CR_IN__ const fpxx_t*   vect,
+  __CR_IN__ uint_t          count
+    )
+{
+    fpxx_t  dvec[7];
+
+    luck_vec(dvec, numb);
+    simx[1] = simx[2] = simx[3] = 0;
+
+    /* 计算与所有历史中奖号码的平均相似度 */
+    for (uint_t idx = 0; idx < count; idx++)
+    {
+        const fpxx_t*   svec = &vect[idx * 7];
+
+        simx[1] += sim_euclidean(svec, dvec, 7);
+        simx[2] += sim_cosine(svec, dvec, 7);
+        simx[3] += sim_pearson(svec, dvec, 7);
+    }
+    simx[1] /= count;
+    simx[2] /= count;
+    simx[3] /= count;
+
+    /* 合成一个综合相似度 */
+    simx[0] = (simx[1] * 2 + simx[2] + simx[3] * 15) / 18;
 }
 
 /*
@@ -220,13 +242,17 @@ int main (int argc, char *argv[])
     int64u  cpu;
     uint_t  sum;
     sint_t  idx;
-    sint_t  cnt = 1;
-    sint_t  min = 0;
-    sint_t  max = 63;
 
     /* 建立 CrHack 系统 */
     if (!set_app_type(CR_APP_CUI))
         return (QST_ERROR);
+
+    fpxx_t  sim[4];
+    sint_t  cnt = 1;
+    sint_t  min = 0;
+    sint_t  max = 63;
+    uint_t  num = 0;
+    fpxx_t* vec = NULL;
 
     /* 参数解析 */
     if (argc > 1)
@@ -241,31 +267,40 @@ int main (int argc, char *argv[])
             {
                 /* 参数3为和值上限 */
                 max = str2intA(argv[3]);
+                if (argc > 4)
+                {
+                    /* 参数4为历史号码文件 */
+                    vec = luck_his(argv[4], &num);
+                }
             }
         }
     }
-
-    /* 数字统计功能 */
-    if (cnt < 1)
-        return (luck_cmp(argv[2]));
 
     /* 参数范围过滤 */
     if      (min <  0) min = 0;
     else if (min > 63) min = 63;
     if      (max <  0) max = 0;
     else if (max > 63) max = 63;
+    rand_seed(timer_get32());
     for (idx = 0; idx < cnt; idx++) {
-        for (mix = 10; ; mix /= 88) {
-            if (mix >= 10000)
-                mix /= 8;
+        for (mix = 188; ; mix /= 88) {
+            if (mix >= 8888)
+                mix %= 8888;
             thread_sleep(mix);
-            sys = timer_get32();
-            cpu = timer_get64();
-            mix = luck_gen(idx + 1, sys, cpu);
+            sys = GetTickCount();
+            cpu = __rdtsc();
+            mix = luck_gen(idx + rand_get(), sys, cpu);
             if (mix > 100000 && luck_flt(mix, min, max, &sum))
                 break;
         }
-        printf("%06u+%u=%u\n", mix / 10, mix % 10, sum);
+        printf("%06u+%u=%u", mix / 10, mix % 10, sum);
+        if (vec != NULL && num != 0) {
+            luck_sim(sim, mix, vec, num);
+            printf(" [%.15f, %.15f, %.15f, %.15f]",
+                    sim[0], sim[1], sim[2], sim[3]);
+        }
+        printf("\n");
     }
+    TRY_FREE(vec);
     return (QST_OKAY);
 }
