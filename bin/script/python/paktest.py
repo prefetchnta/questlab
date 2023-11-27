6dd527559a0baf94744e53db39585ccbef4b6816
prefetchnta <crhackos@gmail.com>
2023-11-27 16:08:44 +0800

    Python: 支持 CHD 文件的校验

--- bin/script/python/paktest.py
+++ bin/script/python/paktest.py
@@ -1,25 +1,27 @@
 #-*- coding: utf-8 -*-
 ##################################
-#   RAR 批量测试脚本
+#   压缩包批量测试脚本
 #
 #   创建时间: 2022-07-18
 ##################################
 
-import os, sys
+import os, sys, time
 
 okays = 0
 count = 0
-flist = "FAILED.TXT"
+flist = "FLIST.TXT"
 if len(sys.argv) < 2:
     zip_cmd_str = '7z.exe t "'
     rar_cmd_str = 'Rar.exe t "'
 else:
     zip_cmd_str = '7z.exe t -p%s "' % sys.argv[1]
     rar_cmd_str = 'Rar.exe t -p%s "' % sys.argv[1]
+chd_cmd_str = 'chdman.exe verify -i "'
 if os.path.exists(flist):
     os.remove(flist)
 os.environ["PATH"] = "C:\\Program Files\\7-Zip;" + os.environ["PATH"]
 os.environ["PATH"] = "C:\\Program Files\\WinRAR;" + os.environ["PATH"]
+os.environ["PATH"] = "D:\\PRJ\\GamePlay\\MAME;" + os.environ["PATH"]
 for dirpath, dirnames, filenames in os.walk("."):
     if len(filenames) == 0:
         continue
@@ -42,14 +44,22 @@ for dirpath, dirnames, filenames in os.walk("."):
             if find_part.count(".7z.001") != 1 and find_part.count(".zip.001") != 1:
                 continue
             cmd_str = zip_cmd_str
+        elif ext == ".chd":
+            cmd_str = chd_cmd_str
         else:
             continue
         full_path = dirpath + "\\" + filename
+        base = time.time()
         if os.system(cmd_str + full_path + '"') == 0:
             okays += 1
+            full_path += " [OKAY] "
         else:
-            fp = open(flist, "a")
-            fp.write(full_path + "\n")
-            fp.close()
+            full_path += " [FAIL] "
+        delta = int(time.time() - base + 0.5)
+        full_path += str(delta) + "S"
+        print(full_path);
+        fp = open(flist, "a")
+        fp.write(full_path + "\n")
+        fp.close()
         count += 1
 print("\nTOTAL:", count, "OKAY:", okays, "FAIL:", count - okays)
