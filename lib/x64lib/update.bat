fed690c136f4cb81afadff69b2f559101cc695f9
prefetchnta <crhackos@gmail.com>
2025-06-26 14:33:45 +0800

    CrHack: 使用 VC2022 编译的库

--- lib/x64lib/update.bat
+++ lib/x64lib/update.bat
@@ -1,11 +1,11 @@
 @echo off
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_CORE.lib
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_FMTZ.lib
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_GFX2.lib
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_GFX3.lib
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_MATH.lib
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_UTIL.lib
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_CORE.lib
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_FMTZ.lib
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_GFX2.lib
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_GFX3.lib
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_MATH.lib
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_UTIL.lib
 cd ..\..\bin\x64bin
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\CrH_*.dll
-copy /Y ..\..\..\FinalBIN\CRHACK\vc2019_win32_x64\GFX?_*.dll
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\CrH_*.dll
+copy /Y ..\..\..\FinalBIN\CRHACK\vc2022_win32_x64\GFX?_*.dll
 pause
